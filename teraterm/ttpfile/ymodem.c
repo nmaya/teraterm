728cc75baad16f7ab870d68befccd076a12c5d12
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-11-01 15:29:23 +0000

    xmodem.c のみで使用する構造体を移動
    - TXVar 構造体を ttftypes.h から xmodem.c 内に移動
    - xmodem.c 内の UI, File I/O の操作を filesys_proto.cpp へ移動
      - 仮実装、後に再度見直す
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/proto_unicode@8974 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/ttpfile/ymodem.c
+++ teraterm/ttpfile/ymodem.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2017 TeraTerm Project
+ * Copyright (C) 2008-2020 TeraTerm Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -49,6 +49,15 @@
 // データ転送サイズ。YMODEMでは 128 or 1024 byte をサポートする。
 #define SOH_DATALEN	128
 #define STX_DATALEN	1024
+
+  /* XMODEM states */
+#define XpktSOH 1
+#define XpktBLK 2
+#define XpktBLK2 3
+#define XpktDATA 4
+
+#define XnakNAK 1
+#define XnakC 2
 
 int YRead1Byte(PFileVarProto fv, PYVar yv, PComVar cv, LPBYTE b)
 {
